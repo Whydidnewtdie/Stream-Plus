03d1b4ff3e467d65030dc5f8326d37f307086bff
Whydidnewtdie <147544144+Whydidnewtdie@users.noreply.github.com>
2023-12-07 14:50:48 +0530

    Add files via upload

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,151 @@
+import React from "react";
+import Container from "react-bootstrap/cjs/Container";
+import Card from "react-bootstrap/cjs/Card";
+import Form from 'react-bootstrap/cjs/Form';
+import Dropdown from "react-bootstrap/cjs/Dropdown";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+let messages = [
+
+
+
+]
+
+class ChatRoom extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'chat': messages,
+            'cur_msg':"",
+            "database": props.database,
+            'name': props.name,
+            "color":"light",
+            "text_color":"dark",
+        };
+        this.chatRef = React.createRef();
+        this.renderAllMessages = this.renderAllMessages.bind(this);
+        this.scrollToBottom = this.scrollToBottom.bind(this);
+        this.setChatColor = this.setChatColor.bind(this);
+
+        setTimeout(()=>{
+            this.state.database.state.ref.onSnapshot(doc => {
+                let data = doc.data();
+                if(data != undefined) {
+                    this.setState({"chat": data['chats']});
+                }
+            })
+
+        },2000)
+    }
+
+    displayMessage(msg,isLast){
+        if(!isLast) {
+            return (
+                <Container>
+                    <Card bg={msg.color} style={{ width: '20rem' }} text={msg.text_color}>
+                        <Card.Header><b>{msg.from}</b></Card.Header>
+                        <Card.Body>{msg.text}</Card.Body>
+                    </Card>
+                    <br/>
+
+                </Container>
+            );
+        } else {
+            return (
+                <div ref={this.chatRef}>
+                <Container>
+                    <Card bg={msg.color} style={{ width: '20rem' }} text={msg.text_color}>
+                        <Card.Header><b>{msg.from}</b></Card.Header>
+                        <Card.Body>{msg.text}</Card.Body>
+                    </Card>
+                    <br/>
+
+                </Container>
+                </div>
+            );
+
+        }
+    }
+
+    renderAllMessages(){
+        let rendered = [];
+
+        this.state.chat.forEach((msg,idx) => {
+            rendered.push(this.displayMessage(msg,(idx == (this.state.chat.length - 1))));
+        })
+
+        console.log(this.chatRef);
+
+        return rendered;//(<div id="chat_div" style={{"overflow-y":"scroll","height": "400px"}} >{rendered}</div>);
+
+    }
+
+    componentDidMount () {
+        console.log("Component mounted");
+        setTimeout(this.scrollToBottom,1500)
+    }
+    componentDidUpdate () {
+        console.log("Component updated");
+        setTimeout(this.scrollToBottom,1500)
+    }
+    scrollToBottom = () => {
+        console.log("Scroll to bottom invoked")
+
+        if(this.state.chat != [] && this.chatRef.current != null) {
+            this.chatRef.current.scrollIntoView({behavior: "smooth"})
+        }
+    }
+
+    setChatColor(color,text_color){
+        this.setState({'color':color,'text_color':text_color});
+    }
+
+    render() {
+        return(
+            <div>
+                <Container fluid>
+                    <div style={{"overflow-y":"scroll","height": "400px"}} >{this.renderAllMessages()}</div>
+                        <Form>
+                            <Form.Group>
+                                <Form.Control as={'textarea'} rows={'2'}
+                                              placeHolder={'Enter Text to send to chat'}
+                                              onChange={(evt)=>this.state.cur_msg = evt.target.value}
+                                              onKeyDown={(evt)=>{
+                                                  if(evt.key == "Enter" && this.state.cur_msg != "" && this.state.cur_msg !="\n"){
+                                                      this.state.database.addChat({"from":this.state.name,"text":this.state.cur_msg,"color":this.state.color,
+                                                          "text_color":this.state.text_color,"time":new Date()});
+                                                      evt.target.value = "";
+                                                  }
+                                              }}
+
+                                />
+                            </Form.Group>
+                        </Form>
+                    <Dropdown>
+                        <Dropdown.Toggle variant={this.state.color} text={this.state.text_color}>
+                            Chat Color
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={()=>this.setChatColor('info','white')}>Blue</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('light','dark')}>White</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('dark','white')}>Black</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('primary','white')}>Dark Blue</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('secondary','white')}>Gray</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('success','white')}>Green</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('danger','white')}>Red</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setChatColor('warning','white')}>Yellow</Dropdown.Item>
+                        </Dropdown.Menu>
+
+                    </Dropdown>
+
+
+
+                </Container>
+
+            </div>
+        );
+    }
+}
+
+export default ChatRoom;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,110 @@
+import React from "react";
+import Button from "react-bootstrap/cjs/Button";
+import Container from "react-bootstrap/cjs/Container";
+import Row from "react-bootstrap/cjs/Row";
+import Col from "react-bootstrap/cjs/Col";
+import Form from "react-bootstrap/cjs/Form";
+import PartyRoom from "../components/PartyRoom";
+
+
+import URLFormatter from "../components/URLFormatter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'url' :'',
+            'room':'',
+            'room_id':'',
+            'name': 'Anonymous',
+            'room_set': false,
+            'title': 'STREAM PLUS',
+            'url_formatter': new URLFormatter(),
+        };
+    }
+
+    render() {
+
+        let pre_room = "";
+        if(!this.state.room_set) {
+            pre_room = (
+                <div>
+                    <br/>
+
+                    <Row className={"justify-content-md-center"}>
+                    <h1 style={{ fontFamily: 'Georgia, serif', fontWeight: 'bold', color: 'white' }}></h1>
+                    </Row>
+
+
+
+                    <Row className={"justify-content-md-center"}>
+                        <Form >
+                            <Form.Group controlId="landingForm.ControlInput1">
+                                <Form.Label className={"form_style"}>Display Name</Form.Label>
+                                <Form.Control onChange={(evt) => this.state.name = evt.target.value}
+                                              placeholder="Anonymous"/>
+                            </Form.Group>
+                            <Form.Group controlId="landingForm.ControlInput2">
+                                <Form.Label className={"form_style"}>Party Title</Form.Label>
+                                <Form.Control onChange={(evt) => this.state.title = evt.target.value}
+                                              placeholder="STREAMPLUS"/>
+                            </Form.Group>
+                            <Form.Group controlId="landingForm.ControlInput1">
+                                <Form.Label className={"form_style"}>Video Address</Form.Label>
+                                <Form.Control onChange={(evt) => this.state.url = evt.target.value}
+                                              placeholder="Video URL"/>
+                            </Form.Group>
+                        </Form>
+                    </Row>
+                    <Row className={"justify-content-md-center"}>
+                        <Button variant={"info"} onClick={() => {
+                            console.log(this.state.url);
+                            this.setState({'room': (<PartyRoom url={this.state.url_formatter.rewrite_url(this.state.url)} title={this.state.title} name={this.state.name}/>),
+                                "room_set":true});
+                        }
+                        }>Start Party</Button>
+                    </Row>
+                    <Row className={"justify-content-md-center"}>
+                        <Form>
+                            <Form.Group controlId="landingForm.ControlInput1">
+                                <Form.Label className={"form_style"}>Room ID</Form.Label>
+                                <Form.Control onChange={(evt) => this.state.room_id = evt.target.value}
+                                              placeholder="Room Id"/>
+                            </Form.Group>
+                        </Form>
+                    </Row>
+                    <Row className={"justify-content-md-center"}>
+                        <Button variant={"info"}  onClick={() => {
+                            console.log(this.state.url);
+                            this.setState({"room_set":true,'room': (<PartyRoom name={this.state.name} room_id={this.state.room_id} is_join={true}/>)});
+                        }
+                        }>Join Party</Button>
+                    </Row>
+                    <br/>
+
+
+                </div>
+
+            );
+        }
+        return(
+            <div>
+                <Container fluid={"md"}>
+
+                    {pre_room}
+                    <Row className={"justify-content-md-center"}>
+                        {this.state.room}
+                    </Row>
+
+
+
+                </Container>
+
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
